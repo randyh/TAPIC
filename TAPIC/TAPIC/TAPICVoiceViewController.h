d932c67a7ca96fef3bed795220a9ebce51dd1ec7
Randy Harper <randyharper@gmail.com>
2014-02-11 21:59:46 -0800

    Push to Talk Button Implementation

---
+++ TAPIC/TAPIC/TAPICVoiceViewController.h
@@ -0,0 +1,20 @@
+//
+//  TAPICVoiceViewController.h
+//  TAPIC
+//
+//  Created by Randy Harper on 2/9/14.
+//  Copyright (c) 2014 Joshua Ferguson, Randy Harper, Pranay Rungta, and Jake Swartz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface TAPICVoiceViewController : UIViewController <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIButton *pushToTalkButton;
+
+- (IBAction)pushToTalkReleased:(id)sender;
+- (IBAction)pushToTalkReleasedOut:(id)sender;
+- (IBAction)pushToTalkDown:(id)sender;
+
+@end
---
+++ TAPIC/TAPIC/TAPICVoiceViewController.m
@@ -0,0 +1,119 @@
+//
+//  TAPICVoiceViewController.m
+//  TAPIC
+//
+//  Created by Randy Harper on 2/9/14.
+//  Copyright (c) 2014 Joshua Ferguson, Randy Harper, Pranay Rungta, and Jake Swartz. All rights reserved.
+//
+
+#import "TAPICVoiceViewController.h"
+
+@interface TAPICVoiceViewController ()
+{
+    AVAudioRecorder *recorder;
+    AVAudioPlayer *player;
+}
+
+@end
+
+@implementation TAPICVoiceViewController
+
+@synthesize pushToTalkButton;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Disable 'Push to Talk' button when application launches
+    [pushToTalkButton setEnabled:NO];
+    
+    // Set the audio file
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"MyAudioMemo.m4a",
+                               nil];
+    NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    
+    // Setup audio session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    
+    // Define the recorder setting
+    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc] init];
+    
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    // Initiate and prepare the recorder
+    recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:NULL];
+    recorder.delegate = self;
+    recorder.meteringEnabled = YES;
+    [recorder prepareToRecord];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)pushToTalkDown:(id)sender
+{
+    // Stop the audio player before recording
+    if (player.playing)
+    {
+        [player stop];
+    }
+    
+    if (!recorder.recording)
+    {
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        [session setActive:YES error:nil];
+        
+        // Start recording
+        [recorder record];
+    }
+}
+
+- (IBAction)pushToTalkReleased:(id)sender
+{
+    if (recorder.recording)
+    {
+        // Stop recording
+        [recorder stop];
+        
+        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+        [audioSession setActive:NO error:nil];
+        
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+        [player setDelegate:self];
+        [player play];
+    }
+}
+
+- (IBAction)pushToTalkReleasedOut:(id)sender
+{
+    [self pushToTalkReleased:sender];
+}
+
+- (void) audioRecorderDidFinishRecording:(AVAudioRecorder *)avrecorder successfully:(BOOL)flag
+{
+    
+}
+
+- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
+{
+
+}
+
+@end
